2b1f07ea3d1844d6d884e451f0d4de90777a0bf6
Mathias Kegelmann <mathias.kegelmann@gmail.com>
2020-12-13 18:26:36 +0100

    Finish Day 12

--- 12.el
+++ 12.el
@@ -23,21 +23,21 @@
                       p
                       (day12-v*
                        (string-to-number arg)
-                             (case (char-after)
-                               (?F d)
-                               (?N '(0 . 1))
-                               (?E '(1 . 0))
-                               (?S '(0  . -1))
-                               (?W '(-1 . 0)))))))
-                  ((?L ?R)
-                   (setq d (if (string= "180" arg)
-                               (day12-v* -1 d)
-                             (day12-v*
-                              (if (string= "270" arg) -1 1)
-                              (cons (* (cdr d)
-                                       (if (= ?L (char-after)) -1  1))
-                                    (* (car d)
-                                       (if (= ?L (char-after))  1 -1)))))))))
+                       (case (char-after)
+                         (?F d)
+                         (?N '(0 . 1))
+                         (?E '(1 . 0))
+                         (?S '(0  . -1))
+                         (?W '(-1 . 0)))))))
+            ((?L ?R)
+             (setq d (if (string= "180" arg)
+                         (day12-v* -1 d)
+                       (day12-v*
+                        (if (string= "270" arg) -1 1)
+                        (cons (* (cdr d)
+                                 (if (= ?L (char-after)) -1  1))
+                              (* (car d)
+                                 (if (= ?L (char-after))  1 -1)))))))))
         (message "%s (%s)" p d)
         (forward-line))
       (list p d (+ (abs (car p)) (abs (cdr p)))))))
@@ -46,3 +46,43 @@
 ((-225 . -296) (0 . 1) 521)
 
 ((17 . -8) (0 . -1) 25)
+
+(defun day12a ()
+  (with-current-buffer "input"
+    (goto-char 1)
+    (let ((wp '(10 . 1))
+          (p '(0 . 0)))
+      (while (not (eobp))
+        (let ((arg (buffer-substring (1+ (point)) (line-end-position))))
+          (case (char-after)
+            (?F
+             (setq p (day12-v+
+                      p
+                      (day12-v* (string-to-number arg) wp))))
+            ((?N ?E ?S ?W)
+             (setq wp (day12-v+
+                       wp
+                       (day12-v*
+                        (string-to-number arg)
+                        (case (char-after)
+                          (?N '(0 . 1))
+                          (?E '(1 . 0))
+                          (?S '(0  . -1))
+                          (?W '(-1 . 0)))))))
+            ((?L ?R)
+             (setq wp (if (string= "180" arg)
+                         (day12-v* -1 wp)
+                       (day12-v*
+                        (if (string= "270" arg) -1 1)
+                        (cons (* (cdr wp)
+                                 (if (= ?L (char-after)) -1  1))
+                              (* (car wp)
+                                 (if (= ?L (char-after))  1 -1)))))))))
+        (message "%s (%s)" p wp)
+        (forward-line))
+      (list p wp (+ (abs (car p)) (abs (cdr p)))))))
+
+(day12a)
+((20290 . -2558) (52 . -31) 22848)
+
+((214 . -72) (4 . -10) 286)
